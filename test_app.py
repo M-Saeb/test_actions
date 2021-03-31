f18d08dd39f18c4fe7dfb3ad0e3279a13e1daca5
M-Saeb <Mohammed.saeb98@hotmail.com>
2021-04-01 00:59:25 +0300

    right value

--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ class Testfunction(unittest.TestCase):
     def test_result(self):
         self.assertAlmostEqual( random_function(2, 3), 6)
         self.assertAlmostEqual( random_function(3, 3), 9)
-        self.assertAlmostEqual( random_function(1, 1), 3)
+        self.assertAlmostEqual( random_function(1, 3), 3)
